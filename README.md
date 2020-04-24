4181a58d7a1272e97915bf516175ca4578860f27
Mateus V. Farias <55674918+fariasmateuss@users.noreply.github.com>
2020-04-24 00:07:00 -0300

    App and update README.md
    update README.md
    
    Remove localstorage

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# product
-👟 Product Card
+<div align="center"> 
+  <img src="docs/resources/thumbnail.png">
+
+  <p>
+    Responsive and animated product card made with HTML, CSS and JavaScript.
+  </p>
+</div>
+
+## Features
+
+- [x] Selection of product colors.
+- [x] Counter added to the product cart.
+
+## Getting Started
+
+Cloning the repository
+
+```
+git clone https://github.com/fariasmateuss/product.git
+```
+
+Open the website on localhost or your network address.
+
+## Contribution
+
+You can send how many PR's do you want, I'll be glad to analyse and accept them! And if you have any question about the project...
+
+Connect with me at [LinkedIn](https://www.linkedin.com/in/fariasmateuss/)
+
+Thank you!
+
+# License
+
+[MIT License](/LICENSE)
--- src/index.js
+++ src/index.js
@@ -5,29 +5,31 @@ const gradients = document.querySelectorAll('.gradient');
 const shoeBg = document.querySelector('.shoeBackground');
 const calculate = document.querySelector('.priceBtn');
 
-let prevColor = "blue";
+let prevColor = 'blue';
 let animationEnd = true;
 
-function changeSize(){
+function changeSize() {
   sizes.forEach(size => size.classList.remove('active'));
   this.classList.add('active');
 }
 
-function changeColor(){
-  if(!animationEnd) return;
-  let primary = this.getAttribute('primary');
-  let color = this.getAttribute('color');
-  let shoe = document.querySelector(`.shoe[color="${color}"]`);
-  let gradient = document.querySelector(`.gradient[color="${color}"]`);
-  let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
+function changeColor() {
+  if (!animationEnd) return;
+  const primary = this.getAttribute('primary');
+  const color = this.getAttribute('color');
+  const shoe = document.querySelector(`.shoe[color='${color}']`);
+  const gradient = document.querySelector(`.gradient[color='${color}']`);
+  const prevGradient = document.querySelector(
+    `.gradient[color='${prevColor}']`
+  );
 
-  if(color == prevColor) return;
+  if (color === prevColor) return;
 
   colors.forEach(c => c.classList.remove('active'));
   this.classList.add('active');
 
   document.documentElement.style.setProperty('--primary', primary);
-  
+
   shoes.forEach(s => s.classList.remove('show'));
   shoe.classList.add('show');
 
@@ -39,30 +41,30 @@ function changeColor(){
   animationEnd = false;
 
   gradient.addEventListener('animationend', () => {
-      animationEnd = true;
-  })
+    animationEnd = true;
+  });
 }
 
 sizes.forEach(size => size.addEventListener('click', changeSize));
 colors.forEach(c => c.addEventListener('click', changeColor));
 
-let x = window.matchMedia("(max-width: 1000px)");
+const x = window.matchMedia('(max-width: 1000px)');
 
-function changeHeight(){
-  if(x.matches){
-    let shoeHeight = shoes[0].offsetHeight;
+function changeHeight() {
+  if (x.matches) {
+    const shoeHeight = shoes[0].offsetHeight;
     shoeBg.style.height = `${shoeHeight * 0.9}px`;
+  } else {
+    shoeBg.style.height = '475px';
   }
-  else{
-    shoeBg.style.height = "475px";
-  }
-}
 
-changeHeight();
+  return;
+}
 
 window.addEventListener('resize', changeHeight);
 
 document.querySelector('.buy').addEventListener('click', () => {
+  event.preventDefault();
   const number = parseInt(calculate.textContent) + 1;
   calculate.textContent = number;
 });
